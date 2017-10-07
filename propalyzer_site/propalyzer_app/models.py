8414b3616ac03a8b7a85a4661d3933c212b64301
zkan <kan@prontomarketing.com>
2017-10-07 17:37:43 +0700

    Add tests for Property model

--- propalyzer_site/propalyzer_app/models.py
+++ propalyzer_site/propalyzer_app/models.py
@@ -187,7 +187,7 @@ class Property(models.Model):
 
     def down_payment_calc(self):
         getcontext().prec = 8
-        dpp = self.down_payment_percentage / Decimal(100)
+        dpp = self.down_payment_percentage / 100.0
         self.down_payment = int((self.curr_value) * (dpp))
         return self.down_payment
 
