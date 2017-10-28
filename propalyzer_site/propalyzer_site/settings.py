6fc05f2aeba0f27c4d505f3d6444dfca41aee4fb
Tom Setliffe <10659165+toms3t@users.noreply.github.com>
2017-10-28 10:08:29 -0700

    Update settings.py

--- propalyzer_site/propalyzer_site/settings.py
+++ propalyzer_site/propalyzer_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Secret.SECRET_KEY  # REPLACE "Secret.SECRET_KEY" WITH ANY STRING
+SECRET_KEY = Secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
