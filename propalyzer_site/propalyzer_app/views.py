fd3140bfb6b05c35a360827281ff47213aae6eb0
Nitsujed <jed9340@gmail.com>
2019-07-05 21:25:08 -0400

    - Split out all the context info into a new script - Cleaned up view file and property file - May be able to clean up property and context file more

--- propalyzer_site/propalyzer_app/views.py
+++ propalyzer_site/propalyzer_app/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from .forms import AddressForm
 from .forms import PropertyForm
 from .property import PropSetup
-from .greatschools import GreatSchools
+from .context_data import ContextData
 
 LOG = logging.getLogger(__name__)
 
@@ -84,90 +84,8 @@ def results(request):
 
     prop_data = request.session.get('prop')
 
-    prop = PropSetup(prop_data['address'])
-    for key in prop_data.keys():
-        prop.__dict__[key] = prop_data[key]
-
-    context = {
-        'address': prop.address,
-        'taxes': '$' + str(int(int(prop.taxes) / 12)),
-        'hoa': '$' + str(int(int(prop.hoa) / 12)),
-        'rent': '$' + str(prop.rent),
-        'vacancy': '$' + str(prop.vacancy_calc),
-        'oper_income': '$' + str(prop.oper_inc_calc),
-        'total_mortgage': '$' + str(prop.total_mortgage_calc),
-        'down_payment_percentage': str(prop.down_payment_percentage) + '%',
-        'down_payment': '$' + str(prop.down_payment_calc),
-        'curr_value': '$' + str(prop.curr_value),
-        'init_cash_invest': '$' + str(prop.init_cash_invested_calc),
-        'oper_exp': '$' + str(prop.oper_exp_calc),
-        'net_oper_income': '$' + str(prop.net_oper_income_calc),
-        'cap_rate': '{0:.1f}%'.format(prop.cap_rate_calc * 100),
-        'initial_market_value': '$' + str(prop.curr_value),
-        'interest_rate': str(prop.interest_rate) + '%',
-        'mort_payment': '$' + str(prop.mort_payment_calc),
-        'sqft': prop.sqft,
-        'closing_costs': '$' + str(prop.closing_costs),
-        'initial_improvements': '$' + str(prop.initial_improvements),
-        'cost_per_sqft': '$' + str(prop.cost_per_sqft_calc),
-        'insurance': '$' + str(int(int(prop.insurance) / 12)),
-        'maintenance': '$' + str(int(int(prop.maint_calc) / 12)),
-        'prop_management_fee': '$' + str(prop.prop_management_fee),
-        'utilities': '$' + str(prop.utilities),
-        'tenant_placement_fee': '$' + str(int(int(prop.tenant_place_calc) / 12)),
-        'resign_fee': '$' + str(int(int(prop.resign_calc) / 12)),
-        'notes': prop.notes,
-        'pub_date': timezone.now,
-        'rtv': '{0:.2f}%'.format(prop.rtv_calc * 100),
-        'cash_flow': '$' + str(prop.cash_flow_calc),
-        'oper_exp_ratio': '{0:.1f}'.format(prop.oper_exp_ratio_calc * 100) + '%',
-        'debt_coverage_ratio': prop.debt_coverage_ratio_calc,
-        'cash_on_cash': '{0:.2f}%'.format(prop.cash_on_cash_calc * 100),
-        'elem_school': prop.schools['elem_school'],
-        'elem_school_score': prop.schools['elem_school_score'],
-        'mid_school': prop.schools['mid_school'],
-        'mid_school_score': prop.schools['mid_school_score'],
-        'high_school': prop.schools['high_school'],
-        'high_school_score': prop.schools['high_school_score'],
-        'year_built': prop.year_built,
-        'county': prop.county,
-        'nat_disasters': 'Unknown',
-        'listing_url': prop.listing_url,
-        'beds': prop.beds,
-        'baths': prop.baths,
-        'livability': prop.areavibes_dict['livability'],
-        'crime': prop.areavibes_dict['crime'],
-        'cost_of_living': prop.areavibes_dict['cost_of_living'],
-        'schools': prop.areavibes_dict['schools'],
-        'employment': prop.areavibes_dict['employment'],
-        'housing': prop.areavibes_dict['housing'],
-        'weather': prop.areavibes_dict['weather'],
-        'disaster1_type': prop.disaster_dict['1'][0],
-        'disaster1_date': prop.disaster_dict['1'][1],
-        'disaster1_county': prop.disaster_dict['1'][2],
-        'disaster1_url': prop.disaster_dict['1'][4],
-        'disaster1_title': prop.disaster_dict['1'][5],
-        'disaster2_type': prop.disaster_dict['2'][0],
-        'disaster2_date': prop.disaster_dict['2'][1],
-        'disaster2_county': prop.disaster_dict['2'][2],
-        'disaster2_url': prop.disaster_dict['2'][4],
-        'disaster2_title': prop.disaster_dict['2'][5],
-        'disaster3_type': prop.disaster_dict['3'][0],
-        'disaster3_date': prop.disaster_dict['3'][1],
-        'disaster3_county': prop.disaster_dict['3'][2],
-        'disaster3_url': prop.disaster_dict['3'][4],
-        'disaster3_title': prop.disaster_dict['3'][5],
-        'disaster4_type': prop.disaster_dict['4'][0],
-        'disaster4_date': prop.disaster_dict['4'][1],
-        'disaster4_county': prop.disaster_dict['4'][2],
-        'disaster4_url': prop.disaster_dict['4'][4],
-        'disaster4_title': prop.disaster_dict['4'][5],
-        'disaster5_type': prop.disaster_dict['5'][0],
-        'disaster5_date': prop.disaster_dict['5'][1],
-        'disaster5_county': prop.disaster_dict['5'][2],
-        'disaster5_url': prop.disaster_dict['5'][4],
-        'disaster5_title': prop.disaster_dict['5'][5],
-    }
+    prop = ContextData()
+    context = prop.set_data(prop_data)
     request.session['PROP'] = prop.__dict__
     return render(request, 'app/results.html', context)
 
