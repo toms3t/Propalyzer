5ae0292736f4e0ecb88d7f5b2c2a30d68719a341
Tom Setliffe <tomset@gmail.com>
2019-07-07 08:55:36 -0700

    working

--- filecopy.sh
+++ filecopy.sh
@@ -4,8 +4,4 @@ cp /home/vsts/work/1/s/settings.py /home/vsts/work/1/s/propalyzer_site/propalyze
 cp /home/vsts/work/1/s/secret.py /home/vsts/work/1/s/propalyzer_site/propalyzer_app/
 cp /home/vsts/work/1/s/wsgi.py /home/vsts/work/1/s/propalyzer_site/propalyzer_site/
 cp /home/vsts/work/1/s/urls.py /home/vsts/work/1/s/propalyzer_site/propalyzer_app/
-echo "site dir below"
-ls /home/vsts/work/1/s/propalyzer_site/propalyzer_site/
-echo "app dir below"
-ls /home/vsts/work/1/s/propalyzer_site/propalyzer_app/
 echo "done!"
