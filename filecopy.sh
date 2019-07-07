a8243d688a4820cf04610eb0e34b16bb2c9aaf20
Tom Setliffe <tomset@gmail.com>
2019-07-07 08:32:51 -0700

    t

--- filecopy.sh
+++ filecopy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Start file copy"
-cp /settings.py /propalyzer_site/propalyzer_app/settings.py
+/home/vsts/work/1/s/filecopy.sh
+cp /home/vsts/work/1/s/settings.py /home/vsts/work/1/s/propalyzer_site/propalyzer_app/settings.py
 echo "done!"
