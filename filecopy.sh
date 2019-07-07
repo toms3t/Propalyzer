6b888bcf92022e3c594c076a0f5c5c3b64cacad2
Tom Setliffe <tomset@gmail.com>
2019-07-07 08:44:05 -0700

    w

--- filecopy.sh
+++ filecopy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "Start file copy"
-cp /home/vsts/work/1/s/settings.py /home/vsts/work/1/s/propalyzer_site/propalyzer_app/settings.py
+cp /home/vsts/work/1/s/settings.py /home/vsts/work/1/s/propalyzer_site/propalyzer_site/
+cp /home/vsts/work/1/s/secret.py /home/vsts/work/1/s/propalyzer_site/propalyzer_app/
+cp /home/vsts/work/1/s/wsgi.py /home/vsts/work/1/s/propalyzer_site/propalyzer_site/
+cp /home/vsts/work/1/s/urls.py /home/vsts/work/1/s/propalyzer_site/propalyzer_app/
+echo "site dir below"
+ls /home/vsts/work/1/s/propalyzer_site/propalyzer_site/
+echo "app dir below"
+ls /home/vsts/work/1/s/propalyzer_site/propalyzer_app/
 echo "done!"
