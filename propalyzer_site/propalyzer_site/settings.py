20dc24bea1d27c55499585cec18a35afa5f42dfb
Tom Setliffe <tomset@gmail.com>
2022-10-16 10:12:13 -0400

    adding whitenoise

--- propalyzer_site/propalyzer_site/settings.py
+++ propalyzer_site/propalyzer_site/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "propalyzer_app.apps.PropalyzerAppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "propalyzer_site.urls"
@@ -156,5 +158,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = "staticfiles"
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- propalyzer_site/propalyzer_site/wsgi.py
+++ propalyzer_site/propalyzer_site/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "propalyzer_site.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
