271ce855215fe47d49d3c68172142a368db1c3df
Tom Setliffe <tomset@gmail.com>
2022-10-16 10:20:08 -0400

    updated settings

--- propalyzer_site/propalyzer_site/settings.py
+++ propalyzer_site/propalyzer_site/settings.py
@@ -44,13 +44,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "propalyzer_site.urls"
